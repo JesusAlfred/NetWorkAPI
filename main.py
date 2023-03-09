8c812ecbef05bd4507b9f5e2817a2c444a0da342
Jesus <navarro.jesusalfredo@gmail.com>
2023-03-08 19:07:53 -0600

    paramenter test

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from typing import Union
+
 app = FastAPI()
 
 @app.get("/test")
 def test():
   print("is working")
+
+@app.get("/ping")
+def ping(ip: str):
+    print("ping to:", ip)
